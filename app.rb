af11330da98cc9f7140a864f74e53ff697caf368
globewalldesk <yo.larrysanger@gmail.com>
2018-04-18 22:33:49 -0400

    minor changes

--- app.rb
+++ app.rb
@@ -7,8 +7,16 @@ require 'sinatra/flash'
 require 'sinatra/activerecord'
 require 'warden'
 require 'bcrypt'
-
 enable :sessions
+
+# require libs; includes warden (authentication) routes.
+Dir['./lib/*.rb'].each { |file| require_relative file }
+# require configurations
+Dir['./config/*.rb'].each { |file| require_relative file }
+# require models
+Dir['./models/*.rb'].each { |file| require_relative file }
+# require routes
+Dir['./routes/*.rb'].each { |file| require_relative file; puts "file = #{file}" }
 
 # modular Sinatra app inherit from Sinatra::Base
 class MyApp < Sinatra::Base
@@ -21,14 +29,6 @@ class MyApp < Sinatra::Base
   # files in static are served on "root"
   set :public_folder, File.dirname(__FILE__) + '/public'
 
-  # require libs; includes warden (authentication) routes.
-  Dir['./lib/*.rb'].each { |file| require_relative file }
-  # require configurations
-  Dir['./config/*.rb'].each { |file| require_relative file }
-  # require models
-  Dir['./models/*.rb'].each { |file| require_relative file }
-  # require routes
-  Dir['./routes/*.rb'].each { |file| require_relative file; puts "file = #{file}" }
-
   run!
 end
+
